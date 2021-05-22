abea490fbc07eb1dd985f72ffa3e2821cb19a1f3
MagzzZiK <kochetoffmaksim@yandex.ru>
2021-05-23 01:15:14 +0300

    some changes task1 in lesson4

---
+++ lesson4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char** argv) { // 1TBS
+   int a
+    int b
+    int sum = a + b;
+    if (sum >= 10 && sum <= 20){
+         std::cout << "true" << std::endl;
+    }
+} else {
+        std::cout << "false" << std::endl;
+    }
+
+    }
+
+    return 0;
+}
