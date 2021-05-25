7b3fd1e8f06988612c8aaa363c45d543de93f9a7
MagzzZiK <kochetoffmaksim@yandex.ru>
2021-05-25 14:21:31 +0300

    1task lesson4

--- dz4/src/lesson4.cpp
+++ dz4/src/lesson4.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 
 int main(int argc, const char** argv) { // 1TBS
-   int a
-    int b
+    int a=12;
+    int b=10;
     int sum = a + b;
-    if (sum >= 10 && sum <= 20){
+    if (sum >= 10 && sum <= 20) {
          std::cout << "true" << std::endl;
-    }
-} else {
+    } else {
         std::cout << "false" << std::endl;
-    }
-
     }
 
     return 0;
